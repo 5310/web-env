a37c0b40c4a7143845a8f5c1583ee4151a2a0f05
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-08-21 17:40:26 +0530

    Configs cleared out. This isn't a public package.

--- jspm.config.js
+++ jspm.config.js
@@ -1,7 +1,4 @@
 SystemJS.config({
-  paths: {
-    "web-env/": "dist/"
-  },
   browserConfig: {
     "paths": {
       "npm:": "/jspm_packages/npm/"
@@ -17,17 +14,7 @@ SystemJS.config({
       "plugin-babel": "npm:systemjs-plugin-babel@0.0.25"
     }
   },
-  transpiler: "plugin-babel",
-  packages: {
-    "web-env": {
-      "main": "index.js",
-      "meta": {
-        "*.js": {
-          "loader": "plugin-babel"
-        }
-      }
-    }
-  }
+  transpiler: "plugin-babel"
 });
 
 SystemJS.config({
